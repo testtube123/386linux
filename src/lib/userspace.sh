f86bb1ea616dfec74f1411089685c590e012af9a
testtube123 <goodaythatismyname@gmail.com>
2024-06-05 22:50:13 -0400

    changed readme abit and deleted stuff I prob won't need :3 x2

--- src/lib/userspace.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-declare -a -r USERSPACE=("musl-1.2.5" "busybox-1.36.1" "dropbear-2022.83")
--- src/test/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#DEBUG="-s -S"
-MEM="7808k"
-
-qemu-system-i386 -M microvm $DEBUG -cpu 486 -m "$MEM" -initrd usr/initramfs_data.cpio.gz  -kernel arch/i386/boot/bzImage -nographic -append "console=ttyS0" -serial mon:stdio
-
